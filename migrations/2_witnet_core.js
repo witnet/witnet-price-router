7307d609bcefec660df5ccda7358a1e32fb19938
Claudia Bartoli Duncan <claudia@stampery.co>
2020-04-29 13:37:54 +0200

    fix: update addresses
    Signed-off-by: Claudia Bartoli Duncan <claudia@stampery.co>

--- migrations/2_witnet_core.js
+++ migrations/2_witnet_core.js
@@ -7,16 +7,16 @@ const Witnet = artifacts.require("Witnet")
 
 const addresses = {
   "goerli": {
-    "BlockRelayProxy": "0xbF36475B8245a51B6f5F4ABeF852725ED302DD9b",
-    "CBOR": "0x26D81f33AaCfa5B68B944C52f98Db9733CcaB3c4",
-    "Witnet": "0x559A2af1F61c3aE990fCC4B370857fB1F95525ca",
-    "WitnetRequestsBoardProxy": "0xFBd67d672C12B130b61320026E5B681f9040bCd3",
+    "BlockRelayProxy": "0x367a7870B0Ca4F70dA5Ce43C5d2046C59Fd7Fe3a",
+    "CBOR": "0x3B63c6170e3e95F2c9c01a3986648E12503af116",
+    "Witnet": "0x57e53fA6E507Bd294EFe1A7eD3b6fa3546FE1b55",
+    "WitnetRequestsBoardProxy": "0x916B67a6D470aF09Fd9379d6C67f8552778346f9",
   },
   "rinkeby": {
-    "BlockRelayProxy": "0x21FC161EB3b958f256bF303dEb32755cFe33bbbd",
-    "CBOR": "0x609217CE0352D0935cA82cE1B1b04A4d54A3A849",
-    "Witnet": "0x36a9B745C5d531C1b51fcD9fB87902b3d1876cFd",
-    "WitnetRequestsBoardProxy": "0x996Be500EBF09537EDde024f70fFdFA55089E939",
+    "BlockRelayProxy": "0xBBB618eC53d29B653324c6186A3ADa9Eca4FEB7C",
+    "CBOR": "0xa3FC7A242e2ceE49f652346741192dcC9bD2C214",
+    "Witnet": "0x466c4119Ea5adC9165cce3BD5c28bcD42D3275D7",
+    "WitnetRequestsBoardProxy": "0x9ad9386e6886957A50D30996e35749eC4566a5d9",
   },
 }
 
