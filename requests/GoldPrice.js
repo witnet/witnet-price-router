39f39ccc6357aeb46d50f9937170e8e185ca7a1d
Gorka Irazoqui <gorka.irazoki@gmail.com>
2020-05-25 17:51:26 +0200

    fix: remove backup from requests

--- requests/GoldPrice.js
+++ requests/GoldPrice.js
@@ -60,7 +60,7 @@ const request = new Witnet.Request()
   .addSource(inversoro) // Use source 4
   .setAggregator(aggregator) // Set the aggregator function
   .setTally(tally) // Set the tally function
-  .setQuorum(4, 1, 2, 5, 70) // Set witness count
+  .setQuorum(2, 70) // Set witness count
   .setFees(10, 1, 1, 1) // Set economic incentives
   .schedule(0) // Make this request immediately solvable
 
