2200ca66c4824d5c7228b97fddfad4f5e14b8578
Mario Cao <mariocao@gmail.com>
2021-04-16 12:13:37 +0200

    test: update test to latest changes in UsingWitnet

--- test/eth.js
+++ test/eth.js
@@ -1,5 +1,5 @@
-const WRB = artifacts.require("MockWitnetRequestsBoard")
-const WRBproxy = artifacts.require("WitnetRequestsBoardProxy")
+const WRB = artifacts.require("WitnetRequestBoard")
+const WRBproxy = artifacts.require("WitnetRequestBoardProxy")
 const ethFeed = artifacts.require("EthUsdPriceFeed")
 const Witnet = artifacts.require("Witnet")
 
@@ -7,16 +7,11 @@ const truffleAssert = require("truffle-assertions")
 
 contract("EthUsdPriceFeed", accounts => {
   describe("WRB test suite", () => {
-    let wrbInstance
-    let wrbProxy
-    let feed
-    const inclPrice = web3.utils.toWei("0.0001", "ether")
-    const resPrice = web3.utils.toWei("0.0001", "ether")
-    const blockPrice = web3.utils.toWei("0.0001", "ether")
-    const requestPrice = web3.utils.toWei("0.0003", "ether")
-    beforeEach(async () => {  
+    let witnet, wrbInstance, wrbProxy, feed
+    const reward = web3.utils.toWei("1", "ether")
+    beforeEach(async () => {
       witnet = await Witnet.new()
-      wrbInstance = await WRB.new()
+      wrbInstance = await WRB.new([accounts[0]])
       wrbProxy = await WRBproxy.new(wrbInstance.address, {
         from: accounts[0],
       })
@@ -25,13 +20,12 @@ contract("EthUsdPriceFeed", accounts => {
     })
 
     it("completes the flow with a correct result", async () => {
-      await feed.requestUpdate(inclPrice, resPrice, blockPrice, {value: requestPrice})
-      id = await feed.lastRequestId()
-      await wrbInstance.reportDrHash(id, "0xAA")
-  
-      await wrbInstance.reportResult(id, "0x1b0020000000000000")
-      let tx = await feed.completeUpdate()
-      let value = await feed.valueFor("0xdfaa6f747f0f012e8f2069d6ecacff25f5cdf0258702051747439949737fc0b5")
+      await feed.requestUpdate({ value: reward })
+      const id = await feed.lastRequestId()
+
+      await wrbInstance.reportResult(id, "0xAA", "0x1b0020000000000000")
+      await feed.completeUpdate()
+      const value = await feed.valueFor("0xdfaa6f747f0f012e8f2069d6ecacff25f5cdf0258702051747439949737fc0b5")
 
       assert.equal(web3.utils.toHex(await feed.lastPrice()), 0x20000000000000)
       assert.equal(await feed.pending(), false)
@@ -41,16 +35,15 @@ contract("EthUsdPriceFeed", accounts => {
     })
 
     it("log an event if errored CBOR decoding", async () => {
-      await feed.requestUpdate(inclPrice, resPrice, blockPrice, {value: requestPrice})
-      id = await feed.lastRequestId()
-      let expectedError = 'Tried to read `uint64` from a `CBOR.Value` with majorType != 0'
-      await wrbInstance.reportDrHash(id, "0xAA")
+      await feed.requestUpdate({ value: reward })
+      const id = await feed.lastRequestId()
+      const expectedError = "Tried to read `uint64` from a `CBOR.Value` with majorType != 0"
 
       // This error message triggers a revert when reading it asErrorMessage
-      await wrbInstance.reportResult(id, "0xD827831851F93800FB3FE6666666666666")
-      let tx = await feed.completeUpdate()
+      await wrbInstance.reportResult(id, "0xAA", "0xD827831851F93800FB3FE6666666666666")
+      const tx = await feed.completeUpdate()
       // check emission of the event and its message correctness
-      let event = tx.receipt.rawLogs.some(l => {
+      const event = tx.receipt.rawLogs.some(l => {
         return web3.utils.hexToAscii(l.data.toString()).includes(expectedError)
       })
       // As we are emiting a string event of bytes that are not in UTF8 format, truffle asserts doesn't detect it
@@ -60,30 +53,29 @@ contract("EthUsdPriceFeed", accounts => {
     })
 
     it("reverts when result not ready", async () => {
-      await feed.requestUpdate(inclPrice, resPrice, blockPrice, {value: requestPrice})
-      let expectedError = 'Found empty buffer when parsing CBOR value'
-      await wrbInstance.reportDrHash(id, "0xAA")
-  
+      await feed.requestUpdate({ value: reward })
+      const expectedError = "Witnet request is not yet resolved by the Witnet network"
+
       // should fail to fetch the result
       await truffleAssert.reverts(
-      feed.completeUpdate(), expectedError)
+        feed.completeUpdate(), expectedError)
 
       assert.equal(await feed.pending(), true)
     })
 
     it("reverts when a DR is already pending", async () => {
-      await feed.requestUpdate(inclPrice, resPrice, blockPrice, {value: requestPrice})
-      let expectedError = 'An update is already pending'
-  
+      await feed.requestUpdate({ value: reward })
+      const expectedError = "Complete pending request before requesting a new one"
+
       // should fail to insert another DR
       await truffleAssert.reverts(
-      feed.requestUpdate(inclPrice, resPrice, blockPrice, {value: requestPrice}), expectedError)
+        feed.requestUpdate({ value: reward }), expectedError)
 
       assert.equal(await feed.pending(), true)
     })
 
     it("should fetch 0, 0, 400 if fetching value for non-correct ID", async () => {
-      let value = await feed.valueFor("0xAA")
+      const value = await feed.valueFor("0xAA")
 
       assert.equal(value[0], 0)
       assert.equal(value[1], 0)
@@ -91,13 +83,11 @@ contract("EthUsdPriceFeed", accounts => {
     })
 
     it("should fetch 0, 0, 404 if no update has completed yet", async () => {
-        let value = await feed.valueFor("0xdfaa6f747f0f012e8f2069d6ecacff25f5cdf0258702051747439949737fc0b5")
+      const value = await feed.valueFor("0xdfaa6f747f0f012e8f2069d6ecacff25f5cdf0258702051747439949737fc0b5")
 
-        assert.equal(value[0], 0)
-        assert.equal(value[1], 0)
-        assert.equal(value[2], 404)
+      assert.equal(value[0], 0)
+      assert.equal(value[1], 0)
+      assert.equal(value[2], 404)
     })
-
   })
-
 })
