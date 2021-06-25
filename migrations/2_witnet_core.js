da286c526f6c8699ff133feb00b17fba000c9259
Guillermo Díaz <guillermo@otherplane.com>
2021-06-25 13:46:40 +0200

    fix: redeploy pricefeeds to new zero-data proxy

--- migrations/2_witnet_core.js
+++ migrations/2_witnet_core.js
@@ -10,7 +10,7 @@ const addresses = {
   "rinkeby": {},
   "conflux": {
     "Witnet": "0x8239dbA17E9E922c62128d4083B8627b3b63f69a",
-    "WitnetProxy": "0x851733Ec551720fF2F6e249114435CD00C5585Da",
+    "WitnetProxy": "0x80786fD4878c98EC22559942e533223CaA8B003F",
   },
 }
 
