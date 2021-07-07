fc4bdd049ede41dfd63ef0d51b0f80fe47cac063
Guillermo Díaz <guillermo@otherplane.com>
2021-07-07 11:05:22 +0200

    feat: flatten multiples files, in all platforms

--- scripts/flatten.js
+++ scripts/flatten.js
@@ -1,23 +1,23 @@
 const exec = require('child_process').exec
-const os = require('os')
 const path = require('path')
 const fs = require('fs')
 
 if (process.argv.length < 3) {
-  console.log("Usage: node flatten.js /path/to/base/solidity/file")
+  console.log("Usage: node flatten.js /path/to/solidity/files/")
   process.exit(1)
 }
 
-const target = process.argv[2]
-const basename = path.basename(target)
+const dirname = path.dirname(process.argv[2])
 
-switch (os.type()) {
-  case "Windows_NT":
-    if (!fs.existsSync(".\\contracts\\flattened\\")) {
-      exec("mkdir contracts\\flattened\\")
-    }
-    exec(`npx truffle-flattener ${target} > .\\contracts\\flattened\\Flattened${basename}`)
-    break;
-  default:
-    exec(`mkdir -p contracts/flattened/ 2>/dev/null; npx truffle-flattener ${target} > contracts/flattened/Flattened${basename}.sol`)
+if (!fs.existsSync(".\\contracts\\flattened\\")) {
+  exec("mkdir contracts\\flattened\\")
 }
+
+const files = fs.readdirSync(dirname).filter(filename => filename.endsWith(".sol"))
+files.forEach(filename => {
+  const target = dirname + "\\" + filename
+  console.log(`Flattening ${target}...`)
+  exec(`npx truffle-flattener ${target} > .\\contracts\\flattened\\Flattened${filename}`)
+})
+
+console.log(`Flattened ${files.length} Solidity files.`)
