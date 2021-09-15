fd8ed6c87bbe174244c47d25c888169cb58119e0
Mario Cao <mariocao@gmail.com>
2021-09-15 11:19:35 +0200

    fix(scripts): use default if `WITNET_EVM_REALM` is undefined

--- scripts/avail-networks.js
+++ scripts/avail-networks.js
@@ -1,9 +1,9 @@
 require("dotenv").config()
 
-const realm = process.env.WITNET_EVM_REALM.toLowerCase() || "default"
+const realm = process.env.WITNET_EVM_REALM || "default"
 const settings = require("../migrations/erc2362.settings")
 
-const networks = Object.keys(settings.networks[realm])
+const networks = Object.keys(settings.networks[realm.toLowerCase()])
 console.log(`\nAvailable networks within realm ${realm.toUpperCase()}:\n`)
 networks.forEach(network => console.log(`  ${network}`))
 console.log()
