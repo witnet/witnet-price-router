a49148cc33b8727ce63c3e70375f738c947e98a5
Guillermo Díaz <guillermo@otherplane.com>
2021-11-16 11:00:40 +0100

    chore: change boba settings

--- migrations/erc2362.settings.js
+++ migrations/erc2362.settings.js
@@ -3,19 +3,10 @@ module.exports = {
     default: {
       ERC2362PriceFeed: "ERC2362PriceFeed",
     },
-    boba: {
-      ERC2362PriceFeed: "ERC2362PriceFeedBoba",
-    },
   },
   constructorParams: {
     default: {
       ERC2362PriceFeed: [],
-    },
-    boba: {
-      ERC2362PriceFeed: [
-        15000000, // _oGAS_PRICE
-        '0x4200000000000000000000000000000000000006'  // _oETH_ERC20
-      ],
     },
   },
   compilers: require("witnet-solidity-bridge/migrations/witnet.settings").compilers,
