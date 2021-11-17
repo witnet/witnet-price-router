e2c35961001bb99d73b5143a3d80a1be1e9553d9
Guillermo Díaz <guillermo@otherplane.com>
2021-11-17 16:46:35 +0100

    feat: add CELO/USD price feed example To be deployed into CELO networks.

---
+++ requests/CeloUsdPrice.js
@@ -0,0 +1,62 @@
+import * as Witnet from "witnet-requests"
+import { REDUCERS } from "witnet-requests/dist/radon/types"
+
+// Retrieve CGLD/USD exchange rate from Coinbase
+const coinbase = new Witnet.Source("https://api.coinbase.com/v2/exchange-rates?currency=USD")
+  .parseJSONMap()
+  .getMap("data")
+  .getMap("rates")
+  .getFloat("CGLD")
+  .power(-1)
+  .multiply(10 ** 6)
+  .round()
+
+// Retrieve CELO/USD price from Bittrex
+const bittrexLast = new Witnet.Source("https://api.bittrex.com/api/v1.1/public/getticker?market=USD-CELO")
+  .parseJSONMap()
+  .getMap("result")
+  .getFloat("Last")
+  .multiply(10 ** 6)
+  .round()
+
+// Retrieves USD price of CELO from the OkEx API (derived from USDT/USD exchange rate)
+const okex = new Witnet.Source("https://www.okex.com/api/index/v3/CELO-USD/constituents")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getMap("data") // Access to the `Map` object at `data` key
+  .getFloat("last") // Get the `Float` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const aggregator = new Witnet.Aggregator({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const tally = new Witnet.Tally({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// This is the Witnet.Request object that needs to be exported
+const request = new Witnet.Request()
+  .addSource(coinbase)
+  .addSource(bittrexLast)
+  .addSource(okex)
+  .setAggregator(aggregator) // Set the aggregator function
+  .setTally(tally) // Set the tally function
+  .setQuorum(10, 70) // Set witness count and minimum consensus percentage
+  .setFees(10 ** 6, 10 ** 6) // Set economic incentives
+  .setCollateral(5 * 10 ** 9) // Require 5 wits as collateral
+
+// Do not forget to export the request object
+export { request as default }
