5d926e695efd8f41d4bee0757f45a36a4fd559d8
Guillermo Díaz <guillermo@otherplane.com>
2021-11-17 17:20:33 +0100

    feat: add CELO/EUR price feed example To be deployed into CELO networks.

--- requests/CeloEurPrice.js
+++ requests/CeloEurPrice.js
@@ -1,8 +1,8 @@
 import * as Witnet from "witnet-requests"
 import { REDUCERS } from "witnet-requests/dist/radon/types"
 
-// Retrieve CGLD/USD exchange rate from Coinbase
-const coinbase = new Witnet.Source("https://api.coinbase.com/v2/exchange-rates?currency=USD")
+// Retrieve CGLD/EUR exchange rate from Coinbase
+const coinbase = new Witnet.Source("https://api.coinbase.com/v2/exchange-rates?currency=EUR")
   .parseJSONMap()
   .getMap("data")
   .getMap("rates")
@@ -11,19 +11,20 @@ const coinbase = new Witnet.Source("https://api.coinbase.com/v2/exchange-rates?c
   .multiply(10 ** 6)
   .round()
 
-// Retrieve CELO/USD price from Bittrex
-const bittrex = new Witnet.Source("https://api.bittrex.com/api/v1.1/public/getticker?market=USD-CELO")
+// Retrieve CELO/EUR last price from Bittrex
+const bittrex = new Witnet.Source("https://api.bittrex.com/api/v1.1/public/getticker?market=EUR-CELO")
   .parseJSONMap()
   .getMap("result")
   .getFloat("Last")
   .multiply(10 ** 6)
   .round()
 
-// Retrieves USD price of CELO from the OkEx API (derived from USDT/USD exchange rate)
-const okex = new Witnet.Source("https://www.okex.com/api/index/v3/CELO-USD/constituents")
+// Retrieves CELO/EUR last price form LCX
+const lcx = new Witnet.Source("https://exchange-api.lcx.com/market/tickers")
   .parseJSONMap() // Parse a `Map` from the retrieved `String`
   .getMap("data") // Access to the `Map` object at `data` key
-  .getFloat("last") // Get the `Float` value associated to the `last` key
+  .getMap("CELO/EUR")
+  .getFloat("lastPrice") // Get the `Float` value associated to the `last` key
   .multiply(10 ** 6) // Use 6 digit precision
   .round() // Cast to integer
 
@@ -51,7 +52,7 @@ const tally = new Witnet.Tally({
 const request = new Witnet.Request()
   .addSource(coinbase)
   .addSource(bittrex)
-  .addSource(okex)
+  .addSource(lcx)
   .setAggregator(aggregator) // Set the aggregator function
   .setTally(tally) // Set the tally function
   .setQuorum(10, 70) // Set witness count and minimum consensus percentage
