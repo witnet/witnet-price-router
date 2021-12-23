2cec4bac031afc7bbddfc7734c8f37a129fca2ff
Guillermo Díaz <guillermo@otherplane.com>
2021-12-23 17:48:52 +0100

    refactor: `lastValue()`

--- contracts/WitnetPriceFeed.sol
+++ contracts/WitnetPriceFeed.sol
@@ -97,6 +97,7 @@ contract WitnetPriceFeed
     /// request that got posted to the Witnet Request Board.
     /// @return _lastPrice Last valid price reported back from the Witnet oracle.
     /// @return _lastTimestamp EVM-timestamp of the last valid price.
+    /// @return _lastDrTxHash Hash of the Witnet Data Request that solved the last valid price.
     /// @return _latestUpdateStatus Status code of the latest update request.
     function lastValue()
         external view
@@ -104,6 +105,7 @@ contract WitnetPriceFeed
         returns (
             int _lastPrice,
             uint _lastTimestamp,
+            bytes32 _lastDrTxHash,
             uint _latestUpdateStatus
         )
     {
@@ -117,6 +119,7 @@ contract WitnetPriceFeed
                     return (
                         int256(int64(witnet.asUint64(_latestResult))),
                         _latestResponse.timestamp,
+                        _latestResponse.drTxHash,
                         200
                     );
                 }
@@ -127,14 +130,15 @@ contract WitnetPriceFeed
                 return (
                     int256(int64(witnet.asUint64(_lastValidResult))),
                     _lastValidResponse.timestamp,
+                    _lastValidResponse.drTxHash,
                     _completed ? 400 : 404
                 );
             }
         }
-        return (0, 0, 404);
+        return (0, 0, 0, 404);
     }
 
-    /// Returns hash of the Witnet Data Request that solved the latest update request.
+    /// Returns identifier of the latest update request posted to the Witnet Request Board.
     /// @dev Returning 0 while the latest update request remains unsolved.
     function latestUpdateDrTxHash()
         external view
@@ -172,8 +176,8 @@ contract WitnetPriceFeed
     /// Returns status code of latest update request posted to the Witnet Request Board:
     /// @dev Status codes:
     /// @dev   - 200: update request was succesfully solved with no errors
-    /// @dev   - 400: update request solved with errors
-    /// @dev   - 404: update request was not yet solved
+    /// @dev   - 400: update request was solved with errors
+    /// @dev   - 404: update request was not solved yet 
     function latestUpdateStatus()
         public view
         virtual override
@@ -196,7 +200,7 @@ contract WitnetPriceFeed
     }
 
     /// Returns `true` if latest update request posted to the Witnet Request Board 
-    /// was not yet solved by the Witnet oracle.
+    /// has not been solved yet by the Witnet oracle.
     function pendingUpdate()
         public view
         virtual override
@@ -210,9 +214,7 @@ contract WitnetPriceFeed
 
     /// Posts a new price update request to the Witnet Request Board. Requires payment of a fee
     /// that depends on the value of `tx.gasprice`. See `estimateUpdateFee(uint256)`.
-    /// Moreover, it will transfer back unused funds if payed a higher fee than the one strictly 
-    /// required by the Witnet Request Board.
-    /// @dev If previous update request was not yet solved, calling this method again enables
+    /// @dev If previous update request was not solved yet, calling this method again allows
     /// @dev upgrading the update fee if called with a higher `tx.gasprice` value.
     function requestUpdate()
         public payable
@@ -246,7 +248,7 @@ contract WitnetPriceFeed
         }
     }
 
-    /// Returns true if this contract implements the interface defined by `interfaceId`. 
+    /// Tells whether this contract implements the interface defined by `interfaceId`. 
     /// @dev See the corresponding https://eips.ethereum.org/EIPS/eip-165#how-interfaces-are-identified[EIP section]
     /// @dev to learn more about how these ids are created.
     function supportsInterface(bytes4 _interfaceId)
