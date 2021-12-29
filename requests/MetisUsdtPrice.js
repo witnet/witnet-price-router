25025c5e20cb77c6b47dc54c83330987b85f602d
Guillermo Díaz <guillermo@otherplane.com>
2021-12-29 09:31:56 +0100

    feat: add Price-METIS/USDT-6

---
+++ requests/MetisUsdtPrice.js
@@ -0,0 +1,77 @@
+import * as Witnet from "witnet-requests"
+
+// Retrieves USDT price of METIS from HOO API
+const hoo = new Witnet.Source("https://api.hoolgd.com/open/innovate/v1/tickers/market")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getArray("data") // Access to the `Map` object at `data` key
+  .filter( 
+    // From all elements in the map,
+    // select the one which "symbol" field
+    // matches "METIS-USDT":
+    new Witnet.Script([ Witnet.TYPES.MAP ])
+      .getString("symbol")
+      .match({ "METIS-USDT": true }, false)
+  )
+  .getMap(0) // Get first (and only) element from the resulting Map
+  .getFloat("price") // Get the `Float` value associated to the `price` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Retrieves USDT price of METIS from the Gate.io API
+const gateio = new Witnet.Source("https://data.gateapi.io/api2/1/ticker/metis_usdt")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getFloat("last") // Get the `Float` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Retrieves USDT price of METIS from the MEXC API
+const mexc = new Witnet.Source("https://www.mexc.com/open/api/v2/market/ticker?symbol=METIS_USDT")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getArray("data") // Access to the `Array` object at `data` key
+  .getMap(0) // Access to the `Map` object at index 0
+  .getString("last") // Get the `String` value associated to the `last` key
+  .asFloat() // Parse `String` as `Float`
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Retrieves USDT price of METIS from the HOTBIT API
+const hotbit = new Witnet.Source("https://api.hotbit.io/api/v1/market.last?market=METISUSDT")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getFloat("result") // Get the `Float` value associated to the `result` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const aggregator = new Witnet.Aggregator({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const tally = new Witnet.Tally({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// This is the Witnet.Request object that needs to be exported
+const request = new Witnet.Request()
+  .addSource(hoo)
+  .addSource(gateio)
+  .addSource(mexc)
+  .addSource(hotbit)
+  .setAggregator(aggregator) // Set the aggregator function
+  .setTally(tally) // Set the tally function
+  .setQuorum(10, 51) // Set witness count and minimum consensus percentage
+  .setFees(10 ** 6, 10 ** 6) // Set economic incentives
+  .setCollateral(5 * 10 ** 9) // Require 5 wits as collateral
+
+// Do not forget to export the request object
+export { request as default }
