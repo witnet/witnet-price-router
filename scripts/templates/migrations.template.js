0b5f4b71e1bcc5e78c98979b8b3e303ec598c616
Guillermo Díaz <guillermo@otherplane.com>
2022-01-26 14:53:46 +0100

    fix: handle dry-run migrations

--- scripts/templates/migrations.template.js
+++ scripts/templates/migrations.template.js
@@ -9,7 +9,8 @@ const witnetRequests = require("../../migrations/witnet.requests")
 const WitnetPriceFeed = artifacts.require("#artifact")
 
 module.exports = async function (deployer, network, _accounts) {
-  network = network.split("-")[0]
+  let isFork = network.split("-")[1] === "fork"
+  network = network.split("-")[0]  
   const realm = utils.getRealmNetworkFromString(network)[0]  
   let witnetAddresses
   try {
@@ -62,7 +63,13 @@ module.exports = async function (deployer, network, _accounts) {
         
         // Write new contract address into 'migrations/addresses.json'
         addresses[realm][network][WitnetPriceFeed.contractName] = WitnetPriceFeed.address
-        fs.writeFileSync("./migrations/addresses.json", JSON.stringify(addresses, null, 4), { flag: 'w+'})
+        if (!isFork) {
+          fs.writeFileSync(
+            "./migrations/addresses.json",
+            JSON.stringify(addresses, null, 4),
+            { flag: 'w+'}
+          )
+        }
       } 
 
       // Otherwise, just update the local artifact file:
@@ -82,7 +89,7 @@ module.exports = async function (deployer, network, _accounts) {
         console.log("   > ERC2362 id:     \t ", erc2362id)
         console.log("   > Router address:\t ", router.address)
         let currentPoller = await router.getPriceFeed(erc2362id)
-        if (utils.isNullAddress(currentPoller) || currentPoller !== WitnetPriceFeed.address) {
+        if (!isFork && (utils.isNullAddress(currentPoller) || currentPoller !== WitnetPriceFeed.address)) {
           let answer = (await utils.prompt(`     ? Substitute current pricefeed at ${currentPoller}? [y/N] `)).toLowerCase().trim()
           if (["y", "yes"].includes(answer)) {
             await router.setPriceFeed(
