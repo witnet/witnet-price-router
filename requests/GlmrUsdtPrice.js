539d654fc18dab5770a2030007cd798406fa0500
Guillermo Díaz <guillermo@otherplane.com>
2022-03-09 11:10:33 +0100

    feat: add Price-GLMR/USDT-6

---
+++ requests/GlmrUsdtPrice.js
@@ -0,0 +1,84 @@
+import * as Witnet from "witnet-requests"
+
+// Retrieve GLMR/USDT-6 price from the Binance HTTP-GET API
+const binance = new Witnet.Source("https://api.binance.com/api/v3/ticker/price?symbol=GLMRUSDT")
+  .parseJSONMap()
+  .getFloat("price")
+  .multiply(10 ** 6)
+  .round()
+
+// Retrieve GLMR/USDT-6 price from the Gate.io HTTP-GET API
+const gateio = new Witnet.Source("https://data.gateapi.io/api2/1/ticker/glmr_usdt")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getFloat("last") // Get the `Float` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Retrieves GLMR/USDT-6 price from the HOTBIT HTTP-GET API
+const hotbit = new Witnet.Source("https://api.hotbit.io/api/v1/market.last?market=GLMRUSDT")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getFloat("result") // Get the `Float` value associated to the `result` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Retrieves GLMR/USDT-6 price from KUCOIN HTTP-GET API
+const kucoin = new Witnet.Source("https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=GLMR-USDT")
+  .parseJSONMap() 
+  .getMap("data")
+  .getFloat("price")
+  .multiply(10 ** 6)
+  .round()
+
+// Retrieves GLMR/USDT-6 price from the MEXC HTTP-GET API
+const mexc = new Witnet.Source("https://www.mexc.com/open/api/v2/market/ticker?symbol=GLMR_USDT")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getArray("data") // Access to the `Array` object at `data` key
+  .getMap(0) // Access to the `Map` object at index 0
+  .getFloat("last") // Get the `String` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Retrieves USD price of CELO from the OkEx API (derived from USDT/USD exchange rate)
+const okex = new Witnet.Source("https://www.okex.com/api/index/v3/GLMR-USDT/constituents")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getMap("data") // Access to the `Map` object at `data` key
+  .getFloat("last") // Get the `Float` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Filters out any value that is more than 1.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const aggregator = new Witnet.Aggregator({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 1.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const tally = new Witnet.Tally({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// This is the Witnet.Request object that needs to be exported
+const request = new Witnet.Request()
+  .addSource(binance)
+  .addSource(gateio)
+  .addSource(hotbit)
+  .addSource(kucoin)
+  .addSource(mexc)
+  .addSource(okex)
+  .setAggregator(aggregator) // Set the aggregator function
+  .setTally(tally) // Set the tally function
+  .setQuorum(10, 51) // Set witness count and minimum consensus percentage
+  .setFees(10 ** 6, 10 ** 6) // Set economic incentives
+  .setCollateral(5 * 10 ** 9) // Require 5 wits as collateral
+
+// Do not forget to export the request object
+export { request as default }
