38918241c4b511c0f8012a834b5a36096064a9de
Guillermo Díaz <guillermo@otherplane.com>
2022-03-10 18:37:56 +0100

    fix: remove 1nch source from KCS/USDT

--- requests/KcsUsdtPrice.js
+++ requests/KcsUsdtPrice.js
@@ -16,12 +16,6 @@ const kucoin = new Witnet.Source("https://api.kucoin.com/api/v1/market/orderbook
   .multiply(10 ** 6)
   .round()
 
-// Retrieves USDT price of KCS from 1INCH API
-const inch = new Witnet.Source("https://api.1inch.exchange/v3.0/1/quote?fromTokenAddress=0xf34960d9d60be18cc1d5afc1a6f012a723a28811&toTokenAddress=0xdac17f958d2ee523a2206206994597c13d831ec7&amount=1000000")
-  .parseJSONMap() 
-  .getFloat("toTokenAmount")
-  .round()
-
 // Retrieve USDT price of KCS from MOJITO SWAP
 const mojito = new Witnet.Source("https://graph.witnet.io/?endpoint=https://thegraph.kcc.network/subgraphs/name/mojito/swap&data=%7B%22query%22%3A%22%7B%5Cn%20%20pair%20%28id%3A%20%5C%220xb3b92d6b2656f9ceb4a381718361a21bf9b82bd9%5C%22%29%20%7B%5Cn%20%20%20%20token0Price%5Cn%20%20%7D%5Cn%7D%22%2C%22variables%22%3Anull%2C%22operationName%22%3Anull%7D")
   .parseJSONMap()
@@ -54,7 +48,6 @@ const tally = new Witnet.Tally({
 const request = new Witnet.Request()
   .addSource(bitmax)
   .addSource(kucoin)
-  .addSource(inch)
   .addSource(mojito)
   .setAggregator(aggregator) // Set the aggregator function
   .setTally(tally) // Set the tally function
