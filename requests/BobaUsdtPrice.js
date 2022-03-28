ae75877a6545fa3842808659374579fb9ec3c92a
Guillermo Díaz <guillermo@otherplane.com>
2022-03-28 18:37:23 +0200

    feat: imporve boba/usdt price feed

--- requests/BobaUsdtPrice.js
+++ requests/BobaUsdtPrice.js
@@ -16,6 +16,14 @@ const gateio = new Witnet.Source("https://data.gateapi.io/api2/1/ticker/boba_usd
   .multiply(10 ** 6) // Use 6 digit precision
   .round() // Cast to integer
 
+// Retrieves BOBA/USDT price from the HUOBI API
+const huobi = new Witnet.Source("https://api.huobi.pro/market/detail/merged?symbol=bobausdt")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getMap("tick") // Access to the `Map` object at index 0
+  .getFloat("close") // Get the `String` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
 // Retrieves USDT price of BOBA from the MEXC API
 const mexc = new Witnet.Source("https://www.mexc.com/open/api/v2/market/ticker?symbol=BOBA_USDT")
   .parseJSONMap() // Parse a `Map` from the retrieved `String`
@@ -30,7 +38,7 @@ const mexc = new Witnet.Source("https://www.mexc.com/open/api/v2/market/ticker?s
 // values that pass the filter.
 const aggregator = new Witnet.Aggregator({
   filters: [
-    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+    [Witnet.Types.FILTERS.deviationStandard, 1.5],
   ],
   reducer: Witnet.Types.REDUCERS.averageMean,
 })
@@ -49,10 +57,11 @@ const tally = new Witnet.Tally({
 const request = new Witnet.Request()
   .addSource(aex)
   .addSource(gateio)
+  .addSource(huobi)
   .addSource(mexc)  
   .setAggregator(aggregator) // Set the aggregator function
   .setTally(tally) // Set the tally function
-  .setQuorum(10, 70) // Set witness count and minimum consensus percentage
+  .setQuorum(10, 51) // Set witness count and minimum consensus percentage
   .setFees(10 ** 6, 10 ** 6) // Set economic incentives
   .setCollateral(5 * 10 ** 9) // Require 5 wits as collateral
 
