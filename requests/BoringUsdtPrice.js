16cbb25152579f4dd72c354e103a4914f8f3cb5c
Guillermo Díaz <guillermo@otherplane.com>
2022-05-12 18:41:10 +0200

    feat: add Price-BORING/USDT-6

---
+++ requests/BoringUsdtPrice.js
@@ -0,0 +1,80 @@
+import * as Witnet from "witnet-requests"
+
+// Retrieves BORING/USDT-6 from the BitMart API:
+const bitmart = new Witnet.Source("https://api-cloud.bitmart.com/spot/v1/ticker?symbol=BORING_USDT")
+  .parseJSONMap()
+  .getMap("data")
+  .getArray("tickers")
+  .getMap(0)
+  .getFloat("last_price")
+  .multiply(10 ** 6)
+  .round()
+
+// Retrieves BORING/USDT-6 price from the Gate.io API
+const gateio = new Witnet.Source("https://data.gateapi.io/api2/1/ticker/boring_usdt")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getFloat("last") // Get the `Float` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Retrieves BORING/USDT-6 price from the HUOBI API
+const huobi = new Witnet.Source("https://api.huobi.pro/market/detail/merged?symbol=boringusdt")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getMap("tick") // Access to the `Map` object at index 0
+  .getFloat("close") // Get the `String` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Retrieves BORING/USDT-6 pricefrom the MEXC API
+const mexc = new Witnet.Source("https://www.mexc.com/open/api/v2/market/ticker?symbol=BORING_USDT")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getArray("data") // Access to the `Array` object at `data` key
+  .getMap(0) // Access to the `Map` object at index 0
+  .getFloat("last") // Get the `String` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Retrieves BORING/USDT-6 price from the OkEx API 
+const okex = new Witnet.Source("https://www.okex.com/api/v5/market/ticker?instId=BORING-USDT")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getArray("data") // Access to the `Map` object at `data` key
+  .getMap(0)
+  .getFloat("last") // Get the `Float` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Filters out any value that is more than 1.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const aggregator = new Witnet.Aggregator({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 1.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const tally = new Witnet.Tally({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// This is the Witnet.Request object that needs to be exported
+const request = new Witnet.Request()
+  .addSource(bitmart)
+  .addSource(gateio)
+  .addSource(huobi)
+  .addSource(mexc)
+  .addSource(okex)
+  .setAggregator(aggregator) // Set the aggregator function
+  .setTally(tally) // Set the tally function
+  .setQuorum(10, 51) // Set witness count and minimum consensus percentage
+  .setFees(10 ** 6, 10 ** 6) // Set economic incentives
+  .setCollateral(5 * 10 ** 9) // Require 5 wits as collateral
+
+// Do not forget to export the request object
+export { request as default }
