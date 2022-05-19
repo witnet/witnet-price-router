47ac707ba6dad194f4a0cd754157ed014236f29b
Guillermo Díaz <guillermo@otherplane.com>
2022-05-19 16:31:46 +0200

    fix(requests): upgrade okex api to v5, add kucoin as new source

--- requests/CfxUsdtPrice.js
+++ requests/CfxUsdtPrice.js
@@ -7,17 +7,26 @@ const binance = new Witnet.Source("https://api.binance.com/api/v3/ticker/price?s
   .multiply(10 ** 6) // Use 6 digit precision
   .round() // Cast to integer
 
-// Retrieves USDT price of CFX from the OkEx API
-const okex = new Witnet.Source("https://www.okex.com/api/index/v3/CFX-USD/constituents")
+// Retrieves USDT price of CFX from the Gate.io API
+const gateio = new Witnet.Source("https://data.gateapi.io/api2/1/ticker/cfx_usdt")
   .parseJSONMap() // Parse a `Map` from the retrieved `String`
-  .getMap("data") // Access to the `Map` object at `data` key
   .getFloat("last") // Get the `Float` value associated to the `last` key
   .multiply(10 ** 6) // Use 6 digit precision
   .round() // Cast to integer
 
-// Retrieves USDT price of CFX from the Gate.io API
-const gateio = new Witnet.Source("https://data.gateapi.io/api2/1/ticker/cfx_usdt")
+// Retrieves CFX/USDT-6 from KUCOIN API
+const kucoin = new Witnet.Source("https://api.kucoin.com/api/v1/market/orderbook/level1?symbol=CFX-USDT")
+  .parseJSONMap() 
+  .getMap("data")
+  .getFloat("price")
+  .multiply(10 ** 6)
+  .round()
+
+// Retrieves USDT price of CFX from the OkEx API
+const okex = new Witnet.Source("https://www.okex.com/api/v5/market/ticker?instId=CFX-USDT")
   .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getArray("data") // Access to the `Map` object at `data` key
+  .getMap(0)
   .getFloat("last") // Get the `Float` value associated to the `last` key
   .multiply(10 ** 6) // Use 6 digit precision
   .round() // Cast to integer
@@ -37,7 +46,7 @@ const mexc = new Witnet.Source("https://www.mexc.com/open/api/v2/market/ticker?s
 // values that pass the filter.
 const aggregator = new Witnet.Aggregator({
   filters: [
-    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+    [Witnet.Types.FILTERS.deviationStandard, 1.5],
   ],
   reducer: Witnet.Types.REDUCERS.averageMean,
 })
@@ -54,13 +63,14 @@ const tally = new Witnet.Tally({
 
 // This is the Witnet.Request object that needs to be exported
 const request = new Witnet.Request()
-  .addSource(binance)
-  .addSource(okex)
+  .addSource(binance)  
   .addSource(gateio)
+  .addSource(kucoin)  
   .addSource(mexc)
+  .addSource(okex)
   .setAggregator(aggregator) // Set the aggregator function
   .setTally(tally) // Set the tally function
-  .setQuorum(10, 70) // Set witness count and minimum consensus percentage
+  .setQuorum(10, 51) // Set witness count and minimum consensus percentage
   .setFees(10 ** 6, 10 ** 6) // Set economic incentives
   .setCollateral(5 * 10 ** 9) // Require 5 wits as collateral
 
