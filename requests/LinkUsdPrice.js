33ab673f368d3e815b3070a656d0f6c0f472a1ff
parodyBit <58690522+parodyBit@users.noreply.github.com>
2022-05-23 06:30:17 -0600

    pfs for annex finance

---
+++ requests/LinkUsdPrice.js
@@ -0,0 +1,62 @@
+import * as Witnet from "witnet-requests"
+
+// Retrieve LINK/USD-6 price from Coinbase
+const coinbase = new Witnet.Source("https://api.coinbase.com/v2/exchange-rates?currency=USD")
+  .parseJSONMap()
+  .getMap("data")
+  .getMap("rates")
+  .getFloat("LINK")
+  .power(-1)
+  .multiply(10 ** 6)
+  .round()
+
+// Retrieve LINK/USD-6 price from Kraken
+const kraken = new Witnet.Source("https://api.kraken.com/0/public/Ticker?pair=LINKUSD")
+  .parseJSONMap()
+  .getMap("result")
+  .getMap("LINKUSD")
+  .getArray("a")
+  .getFloat(0)
+  .multiply(10 ** 6)
+  .round()
+
+// Retrieve LINK/usd price from BitStamp
+const bitstamp = new Witnet.Source("https://www.bitstamp.net/api/v2/ticker/linkusd")
+  .parseJSONMap()
+  .getFloat("last")
+  .multiply(10 ** 6)
+  .round()
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const aggregator = new Witnet.Aggregator({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 1.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const tally = new Witnet.Tally({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// This is the Witnet.Request object that needs to be exported
+const request = new Witnet.Request()
+  .addSource(coinbase)
+  .addSource(kraken)
+  .addSource(bitstamp)
+  .setAggregator(aggregator) // Set the aggregator function
+  .setTally(tally) // Set the tally function
+  .setQuorum(10, 51) // Set witness count and minimum consensus percentage
+  .setFees(10 ** 6, 10 ** 6) // Set economic incentives
+  .setCollateral(5 * 10 ** 9) // Require 5 wits as collateral
+
+// Do not forget to export the request object
+export { request as default }
---
+++ requests/ShibUsdPrice.js
@@ -0,0 +1,62 @@
+import * as Witnet from "witnet-requests"
+
+// Retrieve SHIB/USD-9 price from Coinbase
+const coinbase = new Witnet.Source("https://api.coinbase.com/v2/exchange-rates?currency=USD")
+  .parseJSONMap()
+  .getMap("data")
+  .getMap("rates")
+  .getFloat("SHIB")
+  .power(-1)
+  .multiply(10 ** 9)
+  .round()
+
+// Retrieve SHIB/USD-9 price from Kraken
+const kraken = new Witnet.Source("https://api.kraken.com/0/public/Ticker?pair=SHIBUSD")
+  .parseJSONMap()
+  .getMap("result")
+  .getMap("SHIBUSD")
+  .getArray("a")
+  .getFloat(0)
+  .multiply(10 ** 9)
+  .round()
+
+// Retrieves USD price of SHIB from the Gate.io API
+const gateio = new Witnet.Source("https://data.gateapi.io/api2/1/ticker/shib_usd")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getFloat("last") // Get the `Float` value associated to the `last` key
+  .multiply(10 ** 9) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const aggregator = new Witnet.Aggregator({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 1.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const tally = new Witnet.Tally({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// This is the Witnet.Request object that needs to be exported
+const request = new Witnet.Request()
+  .addSource(coinbase)
+  .addSource(kraken)
+  .addSource(gateio)
+  .setAggregator(aggregator) // Set the aggregator function
+  .setTally(tally) // Set the tally function
+  .setQuorum(10, 51) // Set witness count and minimum consensus percentage
+  .setFees(10 ** 6, 10 ** 6) // Set economic incentives
+  .setCollateral(5 * 10 ** 9) // Require 5 wits as collateral
+
+// Do not forget to export the request object
+export { request as default }
