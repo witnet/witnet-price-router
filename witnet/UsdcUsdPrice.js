d277e794fd2c4946425ea2d6ce3697b300a9205f
Guillermo Díaz <guillermo@otherplane.com>
2022-09-06 18:44:48 +0200

    fix: set tally filter to deviationStandard 2.5

--- witnet/UsdcUsdPrice.js
+++ witnet/UsdcUsdPrice.js
@@ -48,12 +48,12 @@ const aggregator = new Witnet.Aggregator({
   reducer: Witnet.Types.REDUCERS.averageMean,
 })
 
-// Filters out any value that is more than 1.5 times the standard
+// Filters out any value that is more than 2.5 times the standard
 // deviationaway from the average, then computes the average mean of the
 // values that pass the filter.
 const tally = new Witnet.Tally({
   filters: [
-    [Witnet.Types.FILTERS.deviationStandard, 1.5],
+    [Witnet.Types.FILTERS.deviationStandard, 2.5],
   ],
   reducer: Witnet.Types.REDUCERS.averageMean,
 })
