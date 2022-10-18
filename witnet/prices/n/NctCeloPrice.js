d23e591cf9d66aa57715cb793a3b5e71dbcbd58f
Guillermo Díaz <guillermo@otherplane.com>
2022-10-18 10:10:00 +0200

    chore: add padding space

--- witnet/prices/n/NctCeloPrice.js
+++ witnet/prices/n/NctCeloPrice.js
@@ -2,19 +2,19 @@ import * as Witnet from "witnet-requests"
 
 // Retrieve NCT/CELO-6 price from the UniswapV3 DEX API:
 const uniswap = new Witnet.GraphQLSource(
-  "https://api.thegraph.com/subgraphs/name/jesse-sawa/uniswap-celo",
-  `{
-    pool (id: "0xdb24905b1b080f65dedb0ad978aad5c76363d3c6") {
-      token1Price
-    }
-  }`,
-)
-.parseJSONMap()
-.getMap("data")
-.getMap("pool")
-.getFloat("token1Price") // Get the `Float` value associated to the `price` key
-.multiply(10 ** 6) // Use 6 digit precision
-.round() // Cast to integer
+    "https://api.thegraph.com/subgraphs/name/jesse-sawa/uniswap-celo",
+    `{
+      pool (id: "0xdb24905b1b080f65dedb0ad978aad5c76363d3c6") {
+        token1Price
+      }
+    }`,
+  )
+  .parseJSONMap()
+  .getMap("data")
+  .getMap("pool")
+  .getFloat("token1Price") // Get the `Float` value associated to the `price` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
 
 // Filters out any value that is more than 1.5 times the standard
 // deviation away from the average, then computes the average mean of the
