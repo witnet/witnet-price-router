36ab2553efff1307e0f3ced95db915ca83dd04e0
Guillermo Díaz <guillermo@otherplane.com>
2022-11-08 12:49:09 +0100

    chore: fmt!

--- scripts/avail-feeds.js
+++ scripts/avail-feeds.js
@@ -1,17 +1,17 @@
 
 const addresses = require("../migrations/addresses")
 const exec = require("child_process").execSync
-const fs = require('fs');
+const fs = require("fs")
 const queries = require("../migrations/witnet-queries")
 const captions = Object.keys(queries)
 
 exec("pwd")
 
 if (process.argv.length >= 3) {
-  let realm, network, networks = require("../migrations/settings").networks
-  network = process.argv[2].toLowerCase()
+  let networks = require("../migrations/settings").networks
+  let realm; const network = process.argv[2].toLowerCase()
   realm = network.split(".")[0].toLowerCase()
-  if (realm === "ethereum") realm = "default" 
+  if (realm === "ethereum") realm = "default"
   if (!networks[realm]) {
     console.log("Unknown realm:", realm)
     process.exit(1)
@@ -34,18 +34,21 @@ if (process.argv.length >= 3) {
     const feed = key + "Feed"
     const addr = addresses[realm][network][feed]
     if (addr && queries[key]?.bytecode) {
-        const dryrun = exec(`npx witnet-toolkit try-query --hex ${queries[key]?.bytecode} | tail -n 2 | head -n 1 | awk -F: '{ print $2 }' | sed 's/ //g' | tr -d \"│\"`).toString().split('\n')[0]
-        console.log(`${feed}${feed.length > 15 ? "\t\t" : "\t\t\t"}${addr}\t${dryrun}`)
-        output.push({ feed, addr, dryrun})
+      const dryrun = exec(
+        `npx witnet-toolkit try-query --hex ${
+          queries[key]?.bytecode
+        } | tail -n 2 | head -n 1 | awk -F: '{ print $2 }' | sed 's/ //g' | tr -d \"│\"`
+      ).toString().split("\n")[0]
+      console.log(`${feed}${feed.length > 15 ? "\t\t" : "\t\t\t"}${addr}\t${dryrun}`)
+      output.push({ feed, addr, dryrun })
     }
   })
-  fs.writeFile(`.out\\${network}.dryruns.out`, JSON.stringify(output), function(err) {
+  fs.writeFile(`.out\\${network}.dryruns.out`, JSON.stringify(output), function (err) {
     if (err) {
-        console.log(err);
+      console.log(err)
     }
-  });
+  })
   console.log()
-  
 } else {
   console.log("Data feeds ready for deployment:\n")
   captions.forEach(key => {
