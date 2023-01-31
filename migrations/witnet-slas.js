242c11993d23ceb0c95c727f5a5a90f764182b77
Guillermo Díaz <guillermo@otherplane.com>
2023-01-31 11:25:31 +0100

    fix: attend PR review comments

--- migrations/witnet-slas.js
+++ migrations/witnet-slas.js
@@ -2,6 +2,6 @@ module.exports = {
     numWitnesses: 10,                   // 10 witnesses
     witnessCollateral: 15 * 10 ** 9,    // 15 wit
     witnessCommitFee:  0.01 * 10 ** 9,  // 0.01 wit
-    witnessReward:     1.5 * 10 ** 9,   // 1.5 wit
+    witnessReward:     0.15 * 10 ** 9,  // 0.15 wit
     witnessingQuorum:  51,              // 51% quorum
 };
