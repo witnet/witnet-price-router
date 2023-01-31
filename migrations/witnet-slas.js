b3e1d87bf3243e0f91ed2d3688e2f5b5694739a5
Guillermo Díaz <guillermo@otherplane.com>
2023-01-31 11:06:24 +0100

    chore: change pfs sla to 1.5 wit reward and 15 wit collateral

---
+++ migrations/witnet-slas.js
@@ -0,0 +1,7 @@
+module.exports = {
+    numWitnesses: 10,                   // 10 witnesses
+    witnessCollateral: 15 * 10 ** 9,    // 15 wit
+    witnessCommitFee:  0.01 * 10 ** 9,  // 0.01 wit
+    witnessReward:     1.5 * 10 ** 9,   // 1.5 wit
+    witnessingQuorum:  51,              // 51% quorum
+};
