67daa61c8472388d34251260caf91abcb86933b6
Guillermo Díaz <guillermo@otherplane.com>
2023-01-31 10:02:14 +0100

    chore: set witness fee to 1.5 wit on all pfs

--- witnet/prices/b/BobaUsdtPrice.js
+++ witnet/prices/b/BobaUsdtPrice.js
@@ -62,7 +62,7 @@ const request = new Witnet.Request()
   .setAggregator(aggregator) // Set the aggregator function
   .setTally(tally) // Set the tally function
   .setQuorum(10, 51) // Set witness count and minimum consensus percentage
-  .setFees(10 ** 8, 10 ** 7) // Witnessing fee: 0.1 wit; Commit/Reveal fee: 0.01 wit;
+  .setFees(15 * 10 ** 7, 10 ** 7) // Witnessing fee: 0.1 wit; Commit/Reveal fee: 0.01 wit;
   .setCollateral(5 * 10 ** 9) // Require 5 wits as collateral
 
 // Do not forget to export the request object
