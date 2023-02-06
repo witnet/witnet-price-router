2c19a1df2b23d66070b2b90bde6e27cee86aaaf8
Guillermo Díaz <guillermo@otherplane.com>
2023-02-06 12:17:25 +0100

    fix: disable binance.us from usdt/usd

--- witnet/prices/u/UsdtUsdPrice.js
+++ witnet/prices/u/UsdtUsdPrice.js
@@ -54,7 +54,7 @@ const tally = new Witnet.Tally({
 
 // This is the Witnet.Request object that needs to be exported
 const request = new Witnet.Request()
-  .addSource(binance)
+  // .addSource(binance)
   .addSource(bitstamp)
   .addSource(bittrex)
   .addSource(kraken)
