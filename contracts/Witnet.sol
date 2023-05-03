d9ae690eea7220949bc0edcc897333358b79c505
Guillermo Díaz <guillermo@otherplane.com>
2023-05-03 10:40:22 +0200

    chore: upgrade depency to wsb#0.7.x

--- contracts/Witnet.sol
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity >=0.7.0 <0.9.0;
-pragma experimental ABIEncoderV2;
-import "witnet-solidity-bridge/contracts/apps/WitnetPriceRouter.sol";
--- migrations/scripts/1_witnet.js
+++ migrations/scripts/1_witnet.js
@@ -1,7 +1,9 @@
 const addresses = require("witnet-solidity-bridge/migrations/witnet.addresses")
 const utils = require("../../scripts/utils")
 
+const WitnetPriceFeeds = artifacts.require("WitnetPriceFeeds")
 const WitnetPriceRouter  = artifacts.require("WitnetPriceRouter")
+const WitnetRequestBoard = artifacts.require("WitnetRequestBoard")
 
 module.exports = async function (deployer, network, [, from]) {
   const isDryRun = network === "test" || network.split("-")[1] === "fork" || network.split("-")[0] === "develop"
@@ -12,11 +14,15 @@ module.exports = async function (deployer, network, [, from]) {
   if (!isDryRun) {
     try {
       witnetAddresses = addresses[ecosystem][network]
+      WitnetPriceFeeds.address = witnetAddresses.WitnetPriceFeeds
       WitnetPriceRouter.address = witnetAddresses.WitnetPriceRouter
+      WitnetRequestBoard.address = witnetAddresses.WitnetRequestBoard
       const header = "Witnet artifacts:"
-      console.info(header)
-      console.info("=".repeat(header.length))
-      console.info("  ", "> WitnetPriceRouter: ", WitnetPriceRouter.address)
+      console.info("\n  ", header)
+      console.info("  ", ".".repeat(header.length))
+      console.info("  ", "> WitnetPriceFeeds:   ", WitnetPriceFeeds.address)
+      console.info("  ", "> WitnetPriceRouter:  ", WitnetPriceRouter.address)
+      console.info("  ", "> WitnetRequestBoard: ", WitnetRequestBoard.address)
     } catch (e) {
       console.error("Fatal: Witnet Foundation addresses were not provided!")
       console.error(e)
