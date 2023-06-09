e8184c703ac614ad2233c728039e12175dd56432
Guillermo Díaz <guillermo@otherplane.com>
2023-06-09 13:04:01 +0200

    feat: add wit/usdt-6

---
+++ witnet/prices/w/WitUsdtPrice.js
@@ -0,0 +1,62 @@
+import * as Witnet from "witnet-requests"
+import * as WitnetSLA from "../../../../../migrations/witnet-slas"
+
+// Retrieves WIT/USDT-6 from the BitMart API:
+const bitmart = new Witnet.Source("https://api-cloud.bitmart.com/spot/v1/ticker?symbol=WIT_USDT")
+  .parseJSONMap()
+  .getMap("data")
+  .getArray("tickers")
+  .getMap(0)
+  .getFloat("last_price")
+  .multiply(10 ** 6)
+  .round()
+
+// Retrieves WIT/USDT-6 price from the Gate.io API
+const gateio = new Witnet.Source("https://data.gateapi.io/api2/1/ticker/wit_usdt")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getFloat("last") // Get the `Float` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Retrieves BORING/USDT-6 pricefrom the MEXC API
+const mexc = new Witnet.Source("https://www.mexc.com/open/api/v2/market/ticker?symbol=WIT_USDT")
+  .parseJSONMap() // Parse a `Map` from the retrieved `String`
+  .getArray("data") // Access to the `Array` object at `data` key
+  .getMap(0) // Access to the `Map` object at index 0
+  .getFloat("last") // Get the `String` value associated to the `last` key
+  .multiply(10 ** 6) // Use 6 digit precision
+  .round() // Cast to integer
+
+// Filters out any value that is more than 1.4 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const aggregator = new Witnet.Aggregator({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 1.4],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// Filters out any value that is more than 2.5 times the standard
+// deviationaway from the average, then computes the average mean of the
+// values that pass the filter.
+const tally = new Witnet.Tally({
+  filters: [
+    [Witnet.Types.FILTERS.deviationStandard, 2.5],
+  ],
+  reducer: Witnet.Types.REDUCERS.averageMean,
+})
+
+// This is the Witnet.Request object that needs to be exported
+const request = new Witnet.Request()
+  .addSource(bitmart)
+  .addSource(gateio)
+  .addSource(mexc)
+  .setAggregator(aggregator) // Set the aggregator function
+  .setTally(tally) // Set the tally function
+  .setQuorum(WitnetSLA.numWitnesses, WitnetSLA.witnessingQuorum) // Set witness count and minimum consensus percentage
+  .setFees(WitnetSLA.witnessReward, WitnetSLA.witnessCommitFee) // Set witness reward and witness commit fee
+  .setCollateral(WitnetSLA.witnessCollateral) // Set witness collateral
+
+// Do not forget to export the request object
+export { request as default }
